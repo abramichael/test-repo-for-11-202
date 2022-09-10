c218d629b14df38384a3ad74f4182e2555c29720
ma <ma@it.kfu.ru>
2022-09-10 11:38:29 +0300

    Task02 with max of 2 elements added

---
+++ 02 Classwork/Task02.java
@@ -0,0 +1,16 @@
+public class Task02 {
+	
+	public static void main(String[] args) {
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int max = a;
+		
+		if (b > max) {
+			max = b;
+
+		System.out.println(max);
+		
+	}
+	
+}
