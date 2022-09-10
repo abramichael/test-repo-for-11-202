6aaa44d841342cd2d412d6abfd9b58f3a4a9ccdf
ma <ma@it.kfu.ru>
2022-09-10 11:21:11 +0300

    beautify the output of Task01a

--- 02 Classwork/Task01a.java
+++ 02 Classwork/Task01a.java
@@ -5,10 +5,8 @@ public class Task01a {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = a + b;
-		System.out.println(c);
-	
-		
+		System.out.println(a + " + " + b + " = " + c);
+
 	}
-	
-	
+
 }
